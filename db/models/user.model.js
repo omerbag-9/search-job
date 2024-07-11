2bc4ffb98ebce64c54c26f841afd71bb30e0633a
omerbag-9 <omerbagprog@gmail.com>
2024-07-11 16:26:58 +0300

    feat user model

---
+++ db/models/user.model.js
@@ -0,0 +1,51 @@
+import { model, Schema } from "mongoose";
+
+// schema
+const userSchema = new Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    userName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    recoveryEmail:{
+        type:String,
+        required:true
+    },
+    DOB:{
+        type:Schema.Types.Date,
+        required:true
+    },
+    mobileNumber:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+    },
+    emailVerified:{
+        type:Boolean,
+        default:false
+    }
+},{timestamps:true})
+// model
+
+export const User = model('User',userSchema)
